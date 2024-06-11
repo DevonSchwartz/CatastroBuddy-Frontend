8ddd4d5a58e8f9fa1d29fb275d9cf2b32f2b02d9
neha shah <amishah121@utexas.edu>
2024-06-11 15:08:56 -0500

    return item box when add item clicked

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import ItemBox from './components/ItemBox';
 import Login from './components/login';
 import AddItemButton from './components/AddItemButton';
+import EditItem from './components/editItem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AddItemButton />
   </React.StrictMode>
 );
 
