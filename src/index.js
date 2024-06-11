954bd7bf7fd0c6628ba163aa1b883c62d137e009
neha shah <amishah121@utexas.edu>
2024-06-11 11:56:37 -0500

    login page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Welcome from './welcome';
 import reportWebVitals from './reportWebVitals';
+import Login from './login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Welcome />
+    <Login />
   </React.StrictMode>
 );
 
