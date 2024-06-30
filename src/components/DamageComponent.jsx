115ecca6762d4780749b40477b6f25d76c32ecac
Devon Schwartz <devon.s.schwartz@gmail.com>
2024-06-30 16:10:30 -0500

    handle damaged status on server

--- src/components/DamageComponent.jsx
+++ src/components/DamageComponent.jsx
@@ -4,12 +4,13 @@ import Switch from '@mui/material/Switch';
 import Button from '@mui/material/Button';
 import './css/DamageComponent.css'; 
 import { BoxContext } from "../context-providers/BoxContext";
-import { convertToBase64 } from '../utils';
+import { convertToBase64, API_ENDPOINT} from '../utils';
 
 
 
 // Integrated into the item box component for users to toggle
 // a damaged item
+// We keep damaged photo in server even if user toggles back to undamaged
 // index (number): the box index of a household item. Used to update items list
 const DamageComponent = (props) => {
     const {items, setItems} = useContext(BoxContext)
@@ -19,14 +20,48 @@ const DamageComponent = (props) => {
     // needed for dyanmic updates of the damaged image
     const [damagedImage, setDamagedImage] = useState(items?.[index].damagedPhoto) 
 
+    // update the damage status of the item on the server
+    // nextFunction: function to execute on successful response
+    const updateDamage = (nextFunction, newImage) => {
+        const clientId = localStorage.getItem('clientId')
+        const _item_id = items?.[index]._item_id
+
+        fetch (`${API_ENDPOINT}/entry/${clientId}/${_item_id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                itemName: items?.[index].itemName,
+                description: items?.[index].description,
+                price: items?.[index].price,
+                originalPhoto: items?.[index].originalPhoto, 
+                damaged: !newImage ? !items[index].damaged : items[index].damaged,
+                damagedPhoto: newImage ? newImage : items?.[index].damagedPhoto
+            })
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: status ${response.status}`)
+            }
+        })
+        .then(() => {
+            nextFunction()
+        })
+        .catch((error) => { 
+            console.error('There has been a problem with your fetch operation:', error) 
+        })
+    }
 
     // toggle the damaged state of the item
     const handleToggle = () => {
-        setIsDamaged(!isDamaged);
         if (items?.[index]) {
-            items[index].damaged = !items[index].damaged
-            setItems(items)
-            localStorage.setItem('items', JSON.stringify(items));
+            updateDamage(() => {
+                items[index].damaged = !items[index].damaged
+                setIsDamaged(items[index].damaged);
+                setItems(items)
+                localStorage.setItem('items', JSON.stringify(items));
+            }, null)
         }
     };
 
@@ -34,10 +69,12 @@ const DamageComponent = (props) => {
     // image: base64
     const updateDamageImage = (image) => {
         if (items?.[index]) {
-            items[index].damagedPhoto = image
-            setDamagedImage(items[index].damagedPhoto)
-            setItems(items)
-            localStorage.setItem('items', JSON.stringify(items));
+            updateDamage(() => {
+                items[index].damagedPhoto = image
+                setDamagedImage(items[index].damagedPhoto)
+                setItems(items)
+                localStorage.setItem('items', JSON.stringify(items));
+            }, image)
         }
     }
 
